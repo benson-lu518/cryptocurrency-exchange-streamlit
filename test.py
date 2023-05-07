5c683f626ede5021bbad823e962407f23ab53d68
bensonlu <do860518@gmail.com>
2023-05-07 17:52:14 +0800

    delete test.py

--- test.py
+++
@@ -1,9 +0,0 @@
-from dao.HoldingDao import HoldingDao
-from navigation.dashboard_yf import get_market, get_historical
-import yfinance as yf
-
-coin = "BTC" + "-USD"
-stock = yf.Ticker(coin)
-
-historical=stock.history().columns
-print(historical)
